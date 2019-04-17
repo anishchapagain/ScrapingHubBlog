5cfcd2d2268b676f6d97589f6d4dac04740d4b08
anishchapagain <anishchapagain>
2019-04-17 20:04:47 +0545

    css

--- Blog/spiders/blog.py
+++ Blog/spiders/blog.py
@@ -5,31 +5,30 @@ from Blog.items import BlogItem
 class BlogSpider(scrapy.Spider):
     name = "blog"
     allowed_domains = ["blog.scrapinghub.com"]
-    start_urls = ('http://blog.scrapinghub.com/tag/web-scraping',)
+    start_urls = ('https://blog.scrapinghub.com/tag/web-scraping',)
 
-    def parse(self, response):
+    def parseCSS(self, response):
         print("Response Type >>> ", type(response))
-        rows = response.xpath("//div[@class='post-listing']//div[@class='post-item']")
+        rows = response.css(".post-item")
+        print("Ready TO LOOP")
         c=1
         for row in rows:
-            print("Inside Row ")
+            print("Inside Row")
             item = BlogItem()
-
-            item['title'] = row.xpath('div[@class="post-header"]/h2/a/text()').extract_first()
-
-            item['blogUrl'] = row.xpath('div[@class="post-header"]/h2/a/@href').extract_first()
-            item['author_name'] = row.xpath(
-                'div[@class="post-header"]//span[@class="author"]/a/text()').extract_first().strip()
-            item['author_url'] = row.xpath(
-                'div[@class="post-header"]//span[@class="author"]/a/@href').extract_first().strip()
-            item['post_date'] = row.xpath(
-                'div[@class="post-header"]//span[@class="date"]/a/text()').extract_first().strip()
-            item['comments'] = row.xpath('//span[@class="custom_listing_comments"]/a/text()').extract_first().strip()
-            desc=row.xpath('div[@class="post-content"]//p/text()').extract_first()
+            item['title'] = row.css('div.post-header > h2 > a::text').extract_first().strip()
+            item['blogUrl'] = row.css('div.post-header > h2 > a::attr(href)').extract_first().strip()
+            item['author_name'] = row.css('span.author > a::text').extract_first().strip()
+            item['author_url'] = row.css('span.author > a::attr(href)').extract_first().strip()
+            item['post_date'] = row.css('span.date > a::text').extract_first().strip()
+            item['comments'] = row.css('span.custom_listing_comments > a::text').extract_first().strip()
+            desc=row.css('div.post-content > p::text').extract_first().strip()
             print("DESC >> ",desc)
             if desc:
-                item['basic_description'] = row.xpath('div[@class="post-content"]//p/text()').extract_first()
+                item['basic_description'] = desc
             else:
                 item['basic_description'] = ''
             c+=1
             yield item
+
+
+    print('Completed')
