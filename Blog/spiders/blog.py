388305c5b9b12d623087abdd9aec7c87d74008db
anishchapagain <anishchapagain>
2019-04-17 20:39:12 +0545

    added

--- Blog/spiders/blog.py
+++ Blog/spiders/blog.py
@@ -9,26 +9,28 @@ class BlogSpider(scrapy.Spider):
 
     def parse(self, response):
         print("Response Type >>> ", type(response))
-        rows = response.css(".post-item")
+        rows = response.css("div.post-item")
         print("Ready TO LOOP")
         c=1
         for row in rows:
-            print("Inside Row")
-            item = BlogItem()
-            item['title'] = row.css('div.post-header > h2 > a::text').extract_first().strip()
-            item['blogUrl'] = row.css('div.post-header > h2 > a::attr(href)').extract_first().strip()
-            item['author_name'] = row.css('span.author > a::text').extract_first().strip()
-            item['author_url'] = row.css('span.author > a::attr(href)').extract_first().strip()
-            item['post_date'] = row.css('span.date > a::text').extract_first().strip()
-            item['comments'] = row.css('span.custom_listing_comments > a::text').extract_first().strip()
+            print("Inside Row ",c)
+            print(row.css('div.post-header > h2 > a::text').extract_first())
+            print(row.css('div.post-header h2 a::text').extract_first())
+            # item = BlogItem()
+            # item['title'] = row.css('div.post-header > h2 > a::text').extract_first().strip()
+            # item['blogUrl'] = row.css('div.post-header > h2 > a::attr(href)').extract_first().strip()
+            # item['author_name'] = row.css('span.author > a::text').extract_first().strip()
+            # item['author_url'] = row.css('span.author > a::attr(href)').extract_first().strip()
+            # item['post_date'] = row.css('span.date > a::text').extract_first().strip()
+            # item['comments'] = row.css('span.custom_listing_comments > a::text').extract_first().strip()
             desc=row.css('div.post-content > p::text').extract_first().strip()
             print("DESC >> ",desc)
-            if desc:
-                item['basic_description'] = desc
-            else:
-                item['basic_description'] = ''
+            # if desc:
+            #     item['basic_description'] = desc
+            # else:
+            #     item['basic_description'] = ''
             c+=1
-            yield item
+            #yield item
 
 
     print('Completed')
