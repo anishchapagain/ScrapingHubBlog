6f9799a4f553ca23db22bb4e7fa58c5063739bce
anishchapagain <anishchapagain>
2019-04-17 19:54:46 +0545

    check

--- Blog/spiders/blog.py
+++ Blog/spiders/blog.py
@@ -10,11 +10,13 @@ class BlogSpider(scrapy.Spider):
     def parse(self, response):
         print("Response Type >>> ", type(response))
         rows = response.xpath("//div[@class='post-listing']//div[@class='post-item']")
-
+        c=1
         for row in rows:
+            print("Inside Row ")
             item = BlogItem()
 
             item['title'] = row.xpath('div[@class="post-header"]/h2/a/text()').extract_first()
+
             item['blogUrl'] = row.xpath('div[@class="post-header"]/h2/a/@href').extract_first()
             item['author_name'] = row.xpath(
                 'div[@class="post-header"]//span[@class="author"]/a/text()').extract_first().strip()
@@ -23,6 +25,9 @@ class BlogSpider(scrapy.Spider):
             item['post_date'] = row.xpath(
                 'div[@class="post-header"]//span[@class="date"]/a/text()').extract_first().strip()
             item['comments'] = row.xpath('//span[@class="custom_listing_comments"]/a/text()').extract_first().strip()
-            item['basic_description'] = row.xpath('div[@class="post-content"]//p/text()').extract_first()
-
+            desc=row.xpath('div[@class="post-content"]//p/text()').extract_first()
+            print("DESC >> ",desc)
+            if desc:
+                item['basic_description'] = row.xpath('div[@class="post-content"]//p/text()').extract_first()
+            c+=1
             yield item
